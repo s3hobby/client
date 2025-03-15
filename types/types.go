e969303df4df281efbc95951053c496ef519c705
Laurent VERDOÏA <verdoialaurent@gmail.com>
2025-03-15 23:36:32 +0100

    Implement ListBuckets

---
+++ types/types.go
@@ -0,0 +1,19 @@
+package types
+
+type ListAllMyBucketsResult struct {
+	Buckets           []Bucket `xml:">Bucket"`
+	Owner             *Owner
+	ContinuationToken *string
+	Prefix            *string
+}
+
+type Bucket struct {
+	BucketRegion *string
+	CreationDate *string
+	Name         *string
+}
+
+type Owner struct {
+	DisplayName *string
+	ID          *string
+}
